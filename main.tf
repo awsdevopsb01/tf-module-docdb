5ad74f22ffd4492553b49d47b03f0fe567b7c8ad
lokesh.nagireddy <lokesh.nagireddy@accenture.com>
2024-04-14 23:06:53 +0200

    aws_ssm issue fix

--- main.tf
+++ main.tf
@@ -52,7 +52,7 @@ resource "aws_docdb_cluster" "docdb" {
   preferred_backup_window = "07:00-09:00"
   skip_final_snapshot     = true
   db_subnet_group_name = aws_docdb_subnet_group.main.name
-  vpc_security_group_ids = aws_security_group.main.id
+  vpc_security_group_ids = [aws_security_group.main.id]
   db_cluster_parameter_group_name = aws_docdb_cluster_parameter_group.cpg.name
   storage_encrypted = true
   kms_key_id = var.kms_arn
